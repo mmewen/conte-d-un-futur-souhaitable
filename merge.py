3f6b029c74a311aec3733c703de1176492506f69
Mewen <mewenmichel@yahoo.fr>
2020-03-10 13:20:37 +0100

    Fix merge script

--- merge.py
+++ merge.py
@@ -3,6 +3,7 @@ from os.path import isfile, join
 import re
 
 filenames = [f for f in listdir("./") if isfile(join("./", f)) and re.match('\d+ - \w.*\w\.md', f)]
+filenames.sort()
 with open('Conte d\'un futur souhaitable.md', 'w', encoding="utf-8") as outfile:
     for fname in filenames:
         with open(fname, 'r', encoding="utf-8") as infile:
